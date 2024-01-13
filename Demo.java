b352b37925969f7bbb48cbfd09e64cd03c7a42d4
Saurav Pandey <sp4398@gmail.com>
2024-01-13 23:01:41 +0530

    Java Programm

---
+++ Demo.java
@@ -0,0 +1,24 @@
+
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        char x = 'a';
+        
+        switch(x){
+        
+        case 66: System.out.println( "B" +" ");break;
+        
+        case 72: System.out.println( "H"+ " ");break;
+        
+        case 97: System.out.println("a"+ "+");
+        
+        case 89: System.out.println( "Y" + "");break;
+        
+        default: System.out.println( "default");break;
+        
+        }
+        
+        }
+}
