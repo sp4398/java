3b5e2a5e49cc19d331daab1c9b0c6d3974804414
Saurav Pandey <sp4398@gmail.com>
2024-03-16 19:50:38 +0530

    java program

---
+++ Constructor.java
@@ -0,0 +1,11 @@
+class Constructor{
+    String name="Saurav";
+    Constructor(){
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        
+        Constructor c=new Constructor();
+    }
+}
